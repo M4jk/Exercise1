c40f4e90975927eedacbc103a2c13145bfd64f92
MK <209344@edu.p.lodz.pl>
2017-08-01 14:06:30 +0200

    added new lines in main

--- zad2.cpp
+++ zad2.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <cstdlib>
+#include <cctype>
 
 using namespace std;
 
@@ -58,7 +60,7 @@ class Stack {
 		return size;
 	}
 	
-	Stack(const Stack &stack) {			//
+	Stack(const Stack &stack) {
 		capacity = stack.capacity;
 		this->size = stack.size;
 		elements = new int[capacity];
@@ -71,35 +73,75 @@ class Stack {
 		delete [] elements;
 	}
 	
-	void operator + (int value) {	//
+	void operator + (int value) {
 		push(value);
 	}
 	
-	friend ostream & operator << (ostream &wyjscie, const Stack &s);	//
+	friend ostream & operator << (ostream &wyjscie, const Stack &s);
 };
 
-	ostream & operator << (ostream &wyjscie, const Stack &s) {	//
+	ostream & operator << (ostream &wyjscie, const Stack &s) {
 		for(int i = s.size - 1 ; i>=0; i--)
 			cout << s.elements[i] << " ";		
 	}
 
 int main(int argc, char** argv) {
+	
+	int rozmiar = 0, liczba = 0;
+	bool flaga = true;
+	string bufor;
 	try {
-		Stack stos(5);
-		stos + 5;
-		stos + 6;
-		stos + 8;
-		stos + 9;
-	//	stos.show();
-	//	cout << "\nZwrocona wartosc:" << stos.stackPop() << endl;
-	//	stos.show();
-	//	cout << "\nAktualny rozmiar stosu:" << stos.getSize() << endl;
-		Stack stos1(stos);
-	//	stos1.show();
-		cout << stos1;
+	cout << "Podaj rozmiar stosu: ";
+	cin >> rozmiar;
+	
+	if(cin.fail() || rozmiar <= 0) {
+		string error = "Nieprawidlowa wartosc";
+		throw error;
+	}
+	
+	Stack stos(rozmiar);
+	cout << "Dozwolone komendy: push (value), pop, print, size, end - konczy program" << endl;
+	
+	while(1) {
+		flaga = true;
+		cin >> bufor;
+		
+		if(bufor != "push" && bufor != "pop" && bufor != "print" && bufor != "size" && bufor != "end") {
+			string error = "Nieprawidlowa komenda";
+			throw error;
+		}
+		
+		if(bufor == "push") {
+			cin.ignore(10, ' ');
+			cin >> bufor;
+			for(int i = 0; i < bufor.length(); i++) {
+				if(isdigit(bufor[i]) == 0) {
+					flaga = false;
+					string error = "Nieprawidlowa wartosc";
+					throw error;
+				}
+			}	
+			if(flaga) {
+			liczba = atoi(bufor.c_str());
+			stos.push(liczba);
+			}
+		}
+		
+		if(bufor == "pop") stos.stackPop();
+		
+		if(bufor == "print") {
+			cout << "Zawartosc stosu: [ ";
+			stos.show();
+			cout << "]" << endl;
+		}
+		
+		if(bufor == "size") cout << "Aktualny rozmiar: " << stos.getSize() << endl;
+		
+		if(bufor == "end") break;
+	}
 	}
 	catch(string x) {
-		cout << "Blad: " << x;
+		cout << "Wyjatek: " << x;
 	}
 	
 	return 0;
